c10bf56ac9e78c7e514386a90a75b83195b0e36b
anifilm <anifilm02@gmail.com>
2025-05-25 22:45:52 +0900

    study cocos 2025-05-25

--- flappy-bird/assets/Script/BirdControl.ts
+++ flappy-bird/assets/Script/BirdControl.ts
@@ -1,26 +1,41 @@
-import { _decorator, Component, EventTouch, input, Input, Node } from 'cc';
+import { _decorator, Contact2DType, PhysicsSystem2D, Collider2D, RigidBody2D, Component, EventTouch, input, Input, Vec2 } from 'cc';
+import MainControl, { GameStates } from './MainControl';
 const { ccclass, property } = _decorator;
 
 @ccclass('BirdControl')
 export default class BirdControl extends Component {
-    // Speed of bird
     speed: number = 0;
+    mainControl: MainControl = null;
 
     onLoad() {
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+        this.mainControl = this.node.parent.getComponent(MainControl);
     }
 
     start() {
+        const collider = this.getComponent(Collider2D);
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        }
 
+        const rigidbody = this.getComponent(RigidBody2D);
+        if (rigidbody) {
+            rigidbody.gravityScale = 0;
+            rigidbody.linearVelocity = new Vec2(0, 0);
+            rigidbody.angularVelocity = 0;
+        }
     }
 
     update(deltaTime: number) {
+        if (this.mainControl.gameStatus !== GameStates.PLAYING) return;
+
         this.speed -= 0.098;
         const pos = this.node.getPosition();
         pos.y += this.speed;
         this.node.setPosition(pos);
 
-        var angle = (this.speed / 2) * 20;
+        var angle = (this.speed * 0.3) * 30;
         if (angle <= -90) {
             angle = -90;
         }
@@ -31,9 +46,24 @@ export default class BirdControl extends Component {
         this.speed = 3;
     }
 
-    onCollisionEnter(other: Node, self: Node) {
-        // Handle collision with pipes or ground
-        console.log("Collision detected with", other.name);
-        // You can add logic here to handle game over or score updates
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D) {
+        if (this.mainControl.gameStatus !== GameStates.PLAYING) return;
+
+        if (otherCollider.node.name === 'PipeUp' || otherCollider.node.name === 'PipeDown' || otherCollider.node.name === 'Ground') {
+            console.log('Game Over!');
+            this.mainControl.gameOver();
+
+            const rigidbody = this.getComponent(RigidBody2D);
+            if (rigidbody) {
+                rigidbody.gravityScale = 1;
+            }
+        }
+    }
+
+    onEndContact(selfCollider: Collider2D, otherCollider: Collider2D) {
+        if (otherCollider.node.name === 'Point') {
+            this.mainControl.gameScore += 1;
+            this.mainControl.lblScore.string = this.mainControl.gameScore.toString();
+        }
     }
 }
--- flappy-bird/assets/Script/MainControl.ts
+++ flappy-bird/assets/Script/MainControl.ts
@@ -1,19 +1,56 @@
-import { _decorator, Component, Node, Sprite, Camera, Prefab, instantiate, director } from 'cc';
+import { _decorator, Component, Node, Sprite, Camera, Prefab, instantiate, Button, Label, RigidBody2D, Vec2, director } from 'cc';
 const { ccclass, property } = _decorator;
+
+export enum GameStates {
+    READY = 0,
+    PLAYING,
+    GAME_OVER,
+}
 
 @ccclass('MainControl')
 export default class MainControl extends Component {
     @property(Sprite)
     spBg: Sprite[] = [null, null];
 
+    @property(Sprite)
+    spBase: Sprite[] = [null, null];
+
+    @property(Label)
+    lblScore: Label = null;
+
     @property(Prefab)
     pipePrefab: Prefab = null;
 
     pipe: Node[] = [null, null, null];
 
+    gameScore: number = 0;
+
+    spGameStart: Sprite = null;
+    spGameOver: Sprite = null;
+    btnStart: Button = null;
+    btnRetry: Button = null;
+
+    gameStatus: GameStates = GameStates.READY;
+
     // LIFE-CYCLE CALLBACKS:
 
-    //onLoad () {}
+    onLoad() {
+        this.spGameStart = this.node.getChildByName('GameStart').getComponent(Sprite);
+        this.spGameStart.enabled = true;
+        this.btnStart = this.node.getChildByName('StartButton').getComponent(Button);
+        this.btnStart.node.active = true;
+        this.btnStart.node.on(Button.EventType.CLICK, this.onStartButtonClick, this);
+
+        this.spGameOver = this.node.getChildByName('GameOver').getComponent(Sprite);
+        this.spGameOver.node.active = true;
+        this.spGameOver.enabled = false;
+        this.btnRetry = this.node.getChildByName('RetryButton').getComponent(Button);
+        this.btnRetry.node.active = false;
+        this.btnRetry.node.on(Button.EventType.CLICK, this.onStartButtonClick, this);
+
+        this.lblScore.node.active = true;
+        this.lblScore.enabled = false;
+    }
 
     start() {
         // set camera ortho height 256
@@ -24,9 +61,9 @@ export default class MainControl extends Component {
 
         for (let i = 0; i < this.pipe.length; i++) {
             this.pipe[i] = instantiate(this.pipePrefab);
-            this.node.addChild(this.pipe[i]);
+            this.node.getChildByName('Pipe').addChild(this.pipe[i]);
             // set pipe position
-            this.pipe[i].setPosition(200 + 200 * i, 0, 0);
+            this.pipe[i].setPosition(300 + 200 * i, 0, 0);
             var minY = -120;
             var maxY = 120;
             // set pipe Y position randomly
@@ -35,13 +72,25 @@ export default class MainControl extends Component {
     }
 
     update(deltaTime: number) {
+        if (this.gameStatus !== GameStates.PLAYING) return;
+
         // move the background node
         for (let i = 0; i < this.spBg.length; i++) {
             const currentPos = this.spBg[i].node.position;
-            const newX = currentPos.x - 1.0;
+            const newX = currentPos.x - 0.5;
             this.spBg[i].node.setPosition(newX, currentPos.y, currentPos.z);
             if (newX <= -276) {
                 this.spBg[i].node.setPosition(276, currentPos.y, currentPos.z);
+            }
+        }
+
+        // move the base node
+        for (let i = 0; i < this.spBase.length; i++) {
+            const currentPos = this.spBase[i].node.position;
+            const newX = currentPos.x - 2.0;
+            this.spBase[i].node.setPosition(newX, currentPos.y, currentPos.z);
+            if (newX <= -312) {
+                this.spBase[i].node.setPosition(312, currentPos.y, currentPos.z);
             }
         }
 
@@ -53,10 +102,51 @@ export default class MainControl extends Component {
             if (newX <= -170) {
                 this.pipe[i].setPosition(430, currentPos.y, currentPos.z);
                 // set pipe Y position randomly
-                var minY = -120;
+                var minY = -90;
                 var maxY = 120;
                 this.pipe[i].setPosition(this.pipe[i].position.x, minY + Math.random() * (maxY - minY), 0);
             }
         }
+    }
+
+    onStartButtonClick() {
+        this.spGameStart.enabled = false;
+        this.spGameOver.enabled = false;
+
+        this.btnStart.node.active = false;
+        this.btnRetry.node.active = false;
+
+        this.gameStatus = GameStates.PLAYING;
+
+        this.gameScore = 0;
+        this.lblScore.enabled = true;
+        this.lblScore.string = this.gameScore.toString();
+
+        // Reset pipes position
+        for (let i = 0; i < this.pipe.length; i++) {
+            // set pipe position
+            this.pipe[i].setPosition(300 + 200 * i, 0, 0);
+            var minY = -90;
+            var maxY = 120;
+            // set pipe Y position randomly
+            this.pipe[i].setPosition(this.pipe[i].position.x, minY + Math.random() * (maxY - minY), 0);
+        }
+
+        // Reset angle and position of bird
+        const birdNode = this.node.getChildByName('Bird');
+        birdNode.setPosition(0, 0, 0);
+        birdNode.setRotationFromEuler(0, 0, 0);
+        const rigidbody = birdNode.getComponent(RigidBody2D);
+        if (rigidbody) {
+            rigidbody.gravityScale = 0;
+            rigidbody.linearVelocity = new Vec2(0, 0);
+            rigidbody.angularVelocity = 0;
+        }
+    }
+
+    gameOver() {
+        this.gameStatus = GameStates.GAME_OVER;
+        this.spGameOver.enabled = true;
+        this.btnRetry.node.active = true;
     }
 }
